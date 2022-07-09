8f426afb3d8435d2b5a3d114cd275dc5969358b5
holgerchristensen <holger.gott@icloud.com>
2022-07-09 02:30:22 +0200

    allow users to create indices

--- src/indices.rs
+++ src/indices.rs
@@ -3,10 +3,10 @@
 
 /// Index of the page (0-based)
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
-pub struct PdfPageIndex(pub(crate) usize);
+pub struct PdfPageIndex(pub usize);
 /// Index of the layer on the nth page
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
-pub struct PdfLayerIndex(pub(crate) usize);
+pub struct PdfLayerIndex(pub usize);
 
 /// Index of the arbitrary content data
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
